4ddcfbb84a0019a52887baea4e3d1cb2320561ac
Logan Kilpatrick <lok026@g.harvard.edu>
2019-12-04 17:10:42 -0500

    Update send.jl

--- src/send.jl
+++ src/send.jl
@@ -21,3 +21,25 @@ function sendtoslack(data::String, user_endpoint::String)
 
     return (String(r.body))
 end
+
+"""
+    sendattachmenttoslack(data::String, user_endpoint::String)
+Takes in a string of data and the endpoint url and returns the HTTP response.
+The response will be "ok" if the request is sent successfully.
+"""
+function sendattachmenttoslack(data::String, user_endpoint::String)
+    HTTP.setuseragent!("HTTP.jl")
+
+
+    params = Dict("$(data)")
+    base_url = "https://hooks.slack.com"
+
+    endpoint = user_endpoint
+    url = base_url * endpoint
+
+    r = HTTP.post(url,
+                 ["Content-Type" => "application/json"],
+                 JSON.json(params))
+
+    return (String(r.body))
+end
