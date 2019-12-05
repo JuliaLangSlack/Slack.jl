bbf40f208f7c8a86dd0261f438d0646d6701e040
Logan Kilpatrick <lok026@g.harvard.edu>
2019-12-05 12:22:00 -0500

    Added another nested dict for testing/demo purposes.

--- test/runtests.jl
+++ test/runtests.jl
@@ -11,7 +11,11 @@ attachment = Dict("attachments" => [Dict("fallback" => "Required plain-text summ
                                         "author_icon" => "http://flickr.com/icons/bobby.jpg",
                                         "title" => "Slack API Documentation",
                                         "title_link" => "https://api.slack.com/",
-                                        "text" => "Optional text that appears within the attachment"
+                                        "text" => "Optional text that appears within the attachment",
+                                        "fields" => [Dict("title" => "Priority",
+                                            "value" => "High",
+                                            "short" => false
+                                            )]
                                     )])
 
 response = sendtoslack(data, endpoint)
